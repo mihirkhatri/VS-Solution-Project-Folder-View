c09c0085507e83ac0d18e4e4baf12de7b4eec261
mihir <mihirkhatri@googlemail.com>
2017-01-08 17:43:28 +0000

    Initial Commit

---
+++ VS-Solution-Project-Folder-View/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace VS_Solution_Project_Folder_View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FolderPathTextBox = new System.Windows.Forms.TextBox();
+            this.GenerateViewButton = new System.Windows.Forms.Button();
+            this.FolderDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(755, 38);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(67, 23);
+            this.BrowseButton.TabIndex = 0;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select the Folder to create the HTML  View :";
+            // 
+            // FolderPathTextBox
+            // 
+            this.FolderPathTextBox.Enabled = false;
+            this.FolderPathTextBox.Location = new System.Drawing.Point(12, 38);
+            this.FolderPathTextBox.Name = "FolderPathTextBox";
+            this.FolderPathTextBox.Size = new System.Drawing.Size(736, 20);
+            this.FolderPathTextBox.TabIndex = 2;
+            // 
+            // GenerateViewButton
+            // 
+            this.GenerateViewButton.Location = new System.Drawing.Point(625, 67);
+            this.GenerateViewButton.Name = "GenerateViewButton";
+            this.GenerateViewButton.Size = new System.Drawing.Size(197, 23);
+            this.GenerateViewButton.TabIndex = 3;
+            this.GenerateViewButton.Text = "Generate structure view...";
+            this.GenerateViewButton.UseVisualStyleBackColor = true;
+            this.GenerateViewButton.Click += new System.EventHandler(this.GenerateViewButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 100);
+            this.Controls.Add(this.GenerateViewButton);
+            this.Controls.Add(this.FolderPathTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrowseButton);
+            this.Name = "Form1";
+            this.Text = "Solution structure view generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FolderPathTextBox;
+        private System.Windows.Forms.Button GenerateViewButton;
+        private System.Windows.Forms.FolderBrowserDialog FolderDlg;
+    }
+}
+
---
+++ VS-Solution-Project-Folder-View/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Configuration;
+
+namespace VS_Solution_Project_Folder_View
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StringBuilder htmlBuilder = new StringBuilder();
+
+        string[] excludeFolders;
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FolderDlg.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                FolderPathTextBox.Text = FolderDlg.SelectedPath;
+            }
+        }
+
+        
+        private void GenerateViewButton_Click(object sender, EventArgs e)
+        {
+            GenerateViewButton.Enabled = false;
+
+            excludeFolders = ConfigurationSettings.AppSettings["folderToExclude"].Split(',');
+
+            htmlBuilder.Clear();
+
+            htmlBuilder.Append(@"
+                <style>
+                    .projectsolution {color : red;}  
+                    .project {color : blue;}  
+                    .solution {color : green;} 
+                    .solutionname {list-style-type : none; color : gray;}
+                    .projectname {list-style-type : none; color : gray;}
+                    .projectsolution ul {margin-left : 0px;padding-left : 0px;}
+                    .project ul {margin-left : 0px;padding-left : 0px;}
+                    .solution ul {margin-left : 0px;padding-left : 0px;}
+                    
+                    body {font-family : Verdana;}
+                    ul {list-style-type : disc;}
+                </style>
+                <script>
+                    function showhide(e) {
+                        if(e.checked) {
+                            var eles = document.querySelectorAll('.folder');
+                            eles.forEach(x => x.style.display = 'none');
+                        } else {
+                            var eles = document.querySelectorAll('.folder');
+                            eles.forEach(x => x.style.display = 'list-item');
+                        }
+                    }
+                </script>
+                <div>
+                    Show only the Project and Solutions : <input type='checkbox' id='hidefolders' onchange='showhide(this)' />
+                </div>
+            ");
+            SearchRecursiveFolder(FolderPathTextBox.Text);
+
+            string htmlFile = FolderPathTextBox.Text + ".html";
+            File.WriteAllText(htmlFile, htmlBuilder.ToString());
+
+            GenerateViewButton.Enabled = true;
+
+            System.Diagnostics.Process.Start(htmlFile);
+        }
+
+
+        private void SearchRecursiveFolder(string FolderPath)
+        {
+            htmlBuilder.Append("<ul>");
+
+            bool isFolderToExclude = false;
+
+            foreach (string s in excludeFolders)
+            {
+                if (FolderPath.EndsWith(s))
+                {
+                    isFolderToExclude = true;
+                    break;
+                }
+            }
+            if (!isFolderToExclude)
+            {
+                DirectoryInfo di = new DirectoryInfo(FolderPath);
+
+                List<string> projectFiles = di.GetFiles().Where(w => w.Name.EndsWith(".csproj")).Select(s => s.Name).ToList();
+                List<string> solutionFiles = di.GetFiles().Where(w => w.Name.EndsWith(".sln")).Select(s => s.Name).ToList();
+
+                bool isProject = projectFiles.Count() > 0;
+                bool isSolution = solutionFiles.Count() > 0;
+
+                if (isProject && isSolution)
+                {
+                    htmlBuilder.Append("<li class='projectsolution'>" + di.Name);
+                    htmlBuilder.Append("<ul>");
+                    foreach (string s in solutionFiles)
+                    {
+                        htmlBuilder.Append(string.Format("<li class='solutionname'>..{0}</li>",s));
+                    }
+                    foreach (string s in projectFiles)
+                    {
+                        htmlBuilder.Append(string.Format("<li class='projectname'>....{0}</li>",s));
+                    }
+                    htmlBuilder.Append("</ul></li>");
+                }
+                if (isProject)
+                {
+                    htmlBuilder.Append("<li class='project'>" + di.Name);
+                    htmlBuilder.Append("<ul>");
+                    foreach (string s in projectFiles)
+                    {
+                        htmlBuilder.Append(string.Format("<li class='projectname'>....{0}</li>", s));
+                    }
+                    htmlBuilder.Append("</ul></li>");
+                }
+                else if (isSolution)
+                {
+                    htmlBuilder.Append("<li class='solution'>" + di.Name);
+                    htmlBuilder.Append("<ul>");
+                    foreach (string s in solutionFiles)
+                    {
+                        htmlBuilder.Append(string.Format("<li class='solutionname'>..{0}</li>", s));
+                    }
+                    htmlBuilder.Append("</ul></li>");
+                }
+                else
+                {
+                    htmlBuilder.Append("<li class='folder'>" + di.Name + "</li>");
+                }
+
+                foreach (DirectoryInfo d in di.GetDirectories())
+                {
+                    SearchRecursiveFolder(d.FullName);
+                    htmlBuilder.Append("</ul>");
+                }
+            }
+        }
+    }
+}
